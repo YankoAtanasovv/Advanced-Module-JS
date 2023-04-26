cc7bd492a3653c9c2817bd5a471a806832f08713
YankoAtanasovv <122305306+YankoAtanasovv@users.noreply.github.com>
2023-04-26 20:34:08 +0300

    Add files via upload

---
+++ echo.js
@@ -0,0 +1,9 @@
+function solve(text) {
+    console.log(text.length);
+    console.log(text);
+
+
+
+}
+
+solve('Hello, JavaScript!');
---
+++ largestNum.js
@@ -0,0 +1,10 @@
+function largestNum(num1, num2, num3) {
+    let largNumFromThree = Math.max(num1, num2, num3);
+
+    console.log(largNumFromThree);
+
+
+
+}
+
+largestNum('5', '-3', '16');
